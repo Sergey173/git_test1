a603ba6409a70ebb900254ff6d0adb6a22c82fd3
Сергей с <serega-kragov@mail.ru>
2022-07-07 14:38:21 +0300

    test2

--- git_test1/git_test1.cpp
+++ git_test1/git_test1.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello git_test2 World!\n";
+	//why it doen't wont to push?
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
